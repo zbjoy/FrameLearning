0d2837dd4caa2c6d24b1ede5deefc884e38e0075
zbjoy <664266432@qq.com>
2024-05-04 12:51:29 +0800

    add msg.proto generate: msg.pb.h and msg.pb.cpp

--- FrameLearning_Day4/GameBusiness/GameBusiness/GameMsg.cpp
+++ FrameLearning_Day4/GameBusiness/GameBusiness/GameMsg.cpp
@@ -1,1 +1,40 @@
 #include "GameMsg.h"
+#include "msg.pb.h"
+
+GameMsg::GameMsg(MSG_TYPE _type, google::protobuf::Message* _pMsg) : enMsgType(_type), pMsg(_pMsg)
+{
+}
+
+GameMsg::GameMsg(MSG_TYPE _type, std::string _strstream)
+{
+	/* 通过简单工厂构造消息对象 */
+	switch (_type)
+	{
+	case GameMsg::MSG_TYPE_LOGIN_ID_NAME:
+		pMsg = new pb::SyncPid();
+		break;
+	case GameMsg::MSG_TYPE_CHAT_CONTENT:
+		pMsg = new pb::Talk();
+		break;
+	case GameMsg::MSG_TYPE_NEW_POSTION:
+		pMsg = new pb::Position();
+		break;
+	case GameMsg::MSG_TYPE_BROADCAST:
+		pMsg = new pb::BroadCast();
+		break;
+	case GameMsg::MSG_TYPE_LOGOFF_ID_NAME:
+		pMsg = new pb::SyncPid();
+		break;
+	case GameMsg::MSG_TYPE_SRD_POSTION:
+		pMsg = new pb::SyncPlayer();
+		break;
+	default:
+		break;
+	}
+
+}
+
+std::string GameMsg::serialize()
+{
+	return std::string();
+}
--- FrameLearning_Day4/GameBusiness/GameBusiness/GameMsg.h
+++ FrameLearning_Day4/GameBusiness/GameBusiness/GameMsg.h
@@ -1,10 +1,25 @@
 #pragma once
 #include <zinx.h>
+#include <google/protobuf/message.h>
 
 class GameMsg :
     public UserData
 {
 public:
+    google::protobuf::Message* pMsg = NULL;
+    enum MSG_TYPE
+    {
+        MSG_TYPE_LOGIN_ID_NAME = 1,
+        MSG_TYPE_CHAT_CONTENT = 2,
+        MSG_TYPE_NEW_POSTION = 3,
+        MSG_TYPE_BROADCAST = 200,
+        MSG_TYPE_LOGOFF_ID_NAME = 201,
+        MSG_TYPE_SRD_POSTION = 202
+    } enMsgType;
 
+    GameMsg(MSG_TYPE _type, google::protobuf::Message* _pMsg);
+    GameMsg(MSG_TYPE _type, std::string _strstream);
+
+    std::string serialize();
 };
 
