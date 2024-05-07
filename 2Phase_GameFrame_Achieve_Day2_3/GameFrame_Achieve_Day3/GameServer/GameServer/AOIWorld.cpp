6212b8aa3b385d20d136a9f42a7e0f8043713445
zbjoy <664266432@qq.com>
2024-05-07 16:25:11 +0800

    add AOIWorld and achieve function (Add_Payer, Del_Player, GetSrdPlayerPosition)

--- 2Phase_GameFrame_Achieve_Day2_3/GameFrame_Achieve_Day3/GameServer/GameServer/AOIWorld.cpp
+++ 2Phase_GameFrame_Achieve_Day2_3/GameFrame_Achieve_Day3/GameServer/GameServer/AOIWorld.cpp
@@ -4,20 +4,84 @@ AOIWorld::AOIWorld(int _x_begin, int _x_end, int _y_begin, int _y_end, int _x_co
 {
     x_width = (x_end - x_begin) / x_count;
     y_width = (y_end - y_begin) / y_count;
+
+    for (int i = 0; i < x_count * y_count; i++)
+    {
+        // std::list<Grid> tmp;
+        Grid tmp;
+        m_grids.push_back(tmp);
+    }
 }
 
 std::list<Player*> AOIWorld::GetSrdPlayer(Player* _player)
 {
-    return std::list<Player*>();
+    std::list<Player*> ret_list;
+    int x = (int)_player->GetX();
+    int y = (int)_player->GetY();
+    int grid = (x - x_begin) / x_width + (y - y_begin) / y_width * x_count;
+
+    int x_index = (x - x_begin) / x_width;
+    int y_index = (y - y_begin) / y_width;
+
+    if (x_index > 0 && y_index > 0)
+    {
+        std::list<Player*>& cur_list = m_grids[grid - 1 - x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (y_index > 0)
+    {
+        std::list<Player*>& cur_list = m_grids[grid - x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (y_index > 0 && x_index < x_count - 1)
+    {
+        std::list<Player*>& cur_list = m_grids[grid + 1 - x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (x_index > 0)
+    {
+        std::list<Player*>& cur_list = m_grids[grid - 1].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    std::list<Player*>& cur_list = m_grids[grid].m_player_list;
+    ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    if (x_index < x_count - 1)
+    {
+        std::list<Player*>& cur_list = m_grids[grid + 1].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (x_index > 0 && y_index < y_count - 1)
+    {
+        std::list<Player*>& cur_list = m_grids[grid - 1 + x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (y_index < y_count - 1)
+    {
+        std::list<Player*>& cur_list = m_grids[grid + x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    if (x_index < x_count - 1 && y_index < y_count - 1)
+    {
+        std::list<Player*>& cur_list = m_grids[grid + 1 + x_count].m_player_list;
+        ret_list.insert(ret_list.end(), cur_list.begin(), cur_list.end());
+    }
+    return ret_list;
 }
 
 bool AOIWorld::Add_Player(Player* _player)
 {
-     
-    return false;
+    int x = (int)_player->GetX();
+    int y = (int)_player->GetY();
+    int grid = (x - x_begin) / x_width + (y - y_begin) / y_width * x_count;
+    m_grids[grid].m_player_list.push_back(_player);
+    return true;
 }
 
 bool AOIWorld::Del_Player(Player* _player)
 {
-    return false;
+    int x = (int)_player->GetX();
+    int y = (int)_player->GetY();
+    int grid = (x - x_begin) / x_width + (y - y_begin) / y_width * x_count;
+    m_grids[grid].m_player_list.remove(_player);
+    return true;
 }
